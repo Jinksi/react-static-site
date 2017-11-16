b477af42239de34bf9ffa28aadc0d69ccb869cd3
Jinksi <ericjinks@gmail.com>
2017-11-16 13:38:59 +1000

    react-snapshot

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+// replace ReactDOM import with the following line:
+import { render } from 'react-snapshot'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// replace ReactDOM render with react-snapshot render
+render(<App />, document.getElementById('root'))
+registerServiceWorker()
