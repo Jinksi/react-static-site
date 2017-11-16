9d0f0e2b7831bad7c927f69f5e8d982437fa14f3
Jinksi <ericjinks@gmail.com>
2017-11-16 12:13:39 +1000

    react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 import Home from './views/Home'
+import About from './views/About'
+import Nav from './components/Nav'
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <Router>
+        <div>
+          <Nav />
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </div>
+      </Router>
     )
   }
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+import { NavLink } from 'react-router-dom'
+
+const Nav = styled.nav`
+  display: flex;
+  padding: 2rem;
+
+  a {
+    margin-right: 1rem;
+    text-decoration: none;
+    text-transform: uppercase;
+    letter-spacing: 0.1em;
+
+    &:hover,
+    &.active {
+      text-decoration: underline;
+    }
+  }
+`
+
+export default (props) => (
+  <Nav>
+    <NavLink exact to='/'>Home</NavLink>
+    <NavLink to='/about/'>About</NavLink>
+  </Nav>
+)
